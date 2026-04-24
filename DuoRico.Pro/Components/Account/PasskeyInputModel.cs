aba1a48997ebe82c4f298ea46ddcdda774066d5f
Maxjannyfer Malato <Maxjannyfer Malato@DESKTOP-QN54FHO>
2026-04-24 17:03:52 -0300

    Adicionar arquivos de projeto.

---
+++ DuoRico.Pro/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace DuoRico.Pro.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ DuoRico.Pro/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace DuoRico.Pro.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
