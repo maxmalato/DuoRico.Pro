fa7d7c079c9be9a362cbd8479bb34c66bde4a817
Maxjannyfer Malato <max.malato@hotmail.com>
2026-05-07 11:45:11 -0300

    refactor(helpers): extract month capitalization logic to static helper and create the page dashboard.razor

---
+++ DuoRico.Pro/Helpers/DateHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace DuoRico.Pro.Helpers;
+
+public static class DateHelpers
+{
+    public static string GetMonthName(int month)
+    {
+        if (month < 1 || month > 12)
+            return string.Empty;
+
+        var culture = CultureInfo.GetCultureInfo("pt-BR");
+        var monthName = culture.DateTimeFormat.GetMonthName(month);
+
+        return culture.TextInfo.ToTitleCase(monthName);
+    }
+}
